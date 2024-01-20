0f280466fa5e40f6eb2b7fbf4d3bb2958814f412
장혜원 <127714800+janghw0126@users.noreply.github.com>
2024-01-20 13:19:18 +0900

    [Silver IV] Title: 괄호, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.py
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -1,21 +1,18 @@
 import sys
-input = sys.stdin.readline
+input=sys.stdin.readline
+
 T=int(input())
-for i in range(T):
-    stack = []
-		#sys 모듈을 쓸 경우 개행문자도 문자열에 포함되기 때문에 rstrip()을 통해 제거해줌.
-    for i in input().rstrip():
-        if i == "(":
-            stack.append(i)
+
+for _ in range(T):
+    stack=[]
+    for char in input().rstrip():
+        if char=="(":
+            stack.append(char)
         else:
-						#오류가 발생할 가능성이 있는 코드
             try:
                 stack.pop()
-						#에러가 발생했을 경우 시행할 코드
             except:
                 print("NO")
                 break
-                # 괄호를 열기도 전에 닫음
     else:
-				# 스택이 안 비어있는 경우 NO 출력
-        print("NO" if stack else "YES")
+        print("YES" if not stack else "NO")
