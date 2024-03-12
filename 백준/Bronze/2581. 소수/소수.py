485eb788df16b4fce1c0a2438870aad91817423e
장혜원 <127714800+janghw0126@users.noreply.github.com>
2024-03-12 14:15:22 +0900

    [Bronze II] Title: 소수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2581. 소수/소수.py
+++ 백준/Bronze/2581. 소수/소수.py
@@ -1,26 +1,19 @@
-#소수
-#1. 첫째줄에 M, 둘째 줄에 N을 입력받는다.
-#2. 소수인 수들을 정렬하는 배열을 선언한다.
-#3. 반복문을 통해 소수인 경우 append해주고 아닌 경우 그냥 넘어간다.
-#4. 소수인 수들만 모아놓은 배열의 요소들의 합을 구하고 min값을 구한다.
-M=int(input())
-N=int(input())
+M = int(input())
+N = int(input())
 
-prime_number=[]
-sum=0
+is_prime = [True] * (N + 1)
+is_prime[0] = is_prime[1] = False
 
-for i in range(M,N+1):
-    for j in range(2,i+1):
-        if i==j:
-            prime_number.append(i)
-        elif i%j==0:
-            break
+for i in range(2, int(N**(1/2)) + 1):
+    if is_prime[i] == False:
+        continue
+    
+    for j in range(i * i, N + 1, i):
+        is_prime[j] = False
+        
 
-for m in range(0,len(prime_number)):
-    sum+=prime_number[m]
-
-if not prime_number:
+primes = [x for x in range(M, N + 1) if is_prime[x] == True]
+if len(primes) == 0:
     print(-1)
 else:
-    print(sum)
-    print(prime_number[0])
+    print(sum(primes), primes[0])
