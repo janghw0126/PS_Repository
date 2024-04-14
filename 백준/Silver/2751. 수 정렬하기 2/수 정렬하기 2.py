5f5129cb9772bb9442384c236f3f8eaf72f24c06
장혜원 <127714800+janghw0126@users.noreply.github.com>
2024-04-14 16:46:44 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1760 ms, Memory: 76972 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
@@ -1,42 +1,11 @@
-### 합병정렬(nlogn)
 import sys
-input=sys.stdin.readline
 
-n=int(input())
-li=[]
+n = int(input())
+num = []
+for _ in range(n):
+    num.append(int(sys.stdin.readline()))
 
-for i in range(n):
-    li.append(int(input()))
+num.sort()
 
-def sort(arr):
-    if len(arr)<2:
-        return arr
-    
-    mid=len(arr)//2
-    left=sort(arr[:mid])
-    right=sort(arr[mid:])
-
-    return merge(left,right)
-    
-def merge(left,right):
-    new_list=[]
-    i=0
-    j=0
-    
-    while (i<len(left)) & (j<len(right)):
-        if left[i]>right[j]:
-            new_list.append(right[j])
-            j+=1
-        else:
-            new_list.append(left[i])
-            i+=1
-    while (j<len(right)):
-            new_list.append(right[j])
-            j+=1
-    while (i<len(left)):
-            new_list.append(left[i])
-            i+=1
-    return new_list
-    
-for i in sort(li):
+for i in num:
     print(i)
