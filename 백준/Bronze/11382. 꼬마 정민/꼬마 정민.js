e91d847c4691da18d8925d9d66545948fedceed8
장혜원 <janghw232625@gmail.com>
2025-05-26 20:47:53 +0900

    [Bronze V] Title: 꼬마 정민, Time: 92 ms, Memory: 9340 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.js
@@ -0,0 +1,5 @@
+const fs = require("fs");
+const filepath = process.platform === 'linux' ? 'dev/stdin' : './input.txt';
+
+const input = fs.readFileSync(filepath).toString().trim().split(' ');
+console.log(Number(input[0])+Number(input[1])+Number(input[2]));
